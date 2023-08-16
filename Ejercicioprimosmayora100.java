c266c03a6334c69dd043b90a40e7091bc1bd70cb
Alangadeaa <102254187+Alangadeaa@users.noreply.github.com>
2023-08-16 09:58:28 -0300

    Add files via upload

---
+++ Ejercicioprimosmayora100.java
@@ -0,0 +1,23 @@
+package omaiga;
+import java.util.Scanner;
+
+public class omaigaaa {
+	
+	public static void main(String args[]) {
+		
+		int a = 0;
+		Scanner scanner = new Scanner(System.in);
+		
+		
+		while(true) {
+			
+			
+			
+			
+			if(a%a == 1 || 1%a == a) {
+				System.out.print("El numero es mayor a 100 y es primo");
+				return;
+			}
+		}
+	}
+}
