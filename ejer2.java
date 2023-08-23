c131924aca356a813dcf3f557eb42f03d2d8c3bf
FedericoHeredia28 <102254038+FedericoHeredia28@users.noreply.github.com>
2023-08-23 09:48:01 -0300

    Update ejer2.java

--- ejer2.java
+++ ejer2.java
@@ -1,36 +1,39 @@
- import java.util.Scanner;
+import java.util.Scanner;
 
-public class ejer2 {
+public class Ejercicio2 {
 
-	public static void main(String[] args) {
-		System.out.println("Ingrese numeros del 1 al 50 (ambos incluidos) separados por un espacio.");
-		Scanner scanner = new Scanner(System.in);
-		String lineaNumeros = scanner.nextLine();
-		Scanner numerosScanner = new Scanner(lineaNumeros);
-		while (numerosScanner.hasNextInt()) {
-			int numero = numerosScanner.nextInt();
-			if (numero > 50 || numero < 0 ) {
-				System.out.println("Ingrese numeros que no sean mayores de 50 ");
-				return;
-			}
-			
-			if (numero == 0 || numero == 1) System.out.printf("%d ", numero);
-		
-			  //if (numero == 4) System.out.printf("4 2 1");
-			  for (int x = numero ; x > 0; x--) {
-			    if (numero % x == 0 ) {
-			    	
-			    	System.out.printf("%d ", x);
-			    	
-			    }
-			  }
+ public static void main(String[] args) {
+   System.out.println("Ingrese numeros del 1 al 50 (ambos incluidos) separados por un espacio.");
+   Scanner scanner = new Scanner(System.in);
+   String lineaNumeros = scanner.nextLine();
+   Scanner numerosScanner = new Scanner(lineaNumeros);
+   while (numerosScanner.hasNextInt()) {
+     int condicion = 0;
+     int numero = numerosScanner.nextInt();
+     if (numero > 50 || numero < 0 ) {
+       System.out.println("Ingrese numeros que no sean mayores de 50 ");
+       return;
+     }
+     
+    
+       for (int x = numero - 1 ; x > 1; x--) {
+         if (numero % x == 0 ) {
+           if(numero != x && x != 1) condicion = 1;
+           System.out.printf("%d ", x);
+           
+         }
+       } 
+       
 
-			
-			System.out.println("Num " + numero);
-		}
-		
-		numerosScanner.close();
-		scanner.close();
-	}
+     if(condicion == 0) {
+       System.out.println("primo:" + numero);
+     } else {
+       System.out.printf("\n");
+     }
+   }
+   
+   numerosScanner.close();
+   scanner.close();
+ }
 
 }
